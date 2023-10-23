9bb63d4c541519332cceb2aba468fd5cf8959e78
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-23 12:28:10 -0400

    pass tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,9 +1,12 @@
 def oxford_comma(items):
+    # add that "and " to last element
     if len(items) > 1:
         items[-1] = "and " + items[-1]
 
+    # if greater than 2 items
     if len(items) > 2:
         return ', '.join(items)
+    # if less than two items
     else:
         return ' '.join(items)
 
